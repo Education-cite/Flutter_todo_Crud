221b3a21ed19186635f523d813e4bd0ecc2ba204
Education-cite <tusarjobs@gmail.com>
2023-03-26 20:00:52 +0600

    f

--- lib/Services/globals.dart
+++ lib/Services/globals.dart
@@ -1,2 +1,2 @@
-const String baseURL = "http://192.168.0.107/tasks";
+const String baseURL = "http://192.168.0.107:8080/tasks";
 const Map<String, String> headers = {"Content-Type": "application/json"};
